54794273119ae9bfbeeeddba39e97ca3d9a93c0e
C-R4Y0-N <130323486+C-R4Y0-N@users.noreply.github.com>
2023-08-12 14:21:43 -0600

    12_08_2023

---
+++ hayqueserminero.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+/*
+    Matriz
+    1 3 2 1  
+    2 5 7 0
+    3 2 4 1
+    8 7 1 1
+*/
+using namespace std;
+int main()
+//{
+//    int n,i=0,sum,x,y;
+//    cin>>n;
+//    int Matriz[3][4]={{2,5,7,0},{3,2,4,1},{8,7,1,1}};
+//    while(n!=Matriz[0][i])
+//    {
+//        i++;
+//    }
+//    x=i;
+//    y=0;
+//        while(y<=5)
+//        {
+//            if(Matriz[x+1][y+1]>Matriz[x][y])
+//            {
+//                sum=Matriz[x+1][y+1]+Matriz[x][y];
+//                x+=1;
+//                y+=1;
+//            }
+//            else if(Matriz[x-1][y-1]>Matriz[x][y])
+//            {
+//                sum=Matriz[x-1][y-1]+Matriz[x][y];
+//                x-=1;
+//                y-=1;
+//            }
+//            else if (Matriz[x+1][y]>Matriz[x][y])
+//            {
+//                sum=Matriz[x+1][y]>Matriz[x][y];
+//                x+=1;
+//            }
+//        }
+//    cout<<sum;
+//    return 0;
+//}
+{
+    return 0;   
+}
